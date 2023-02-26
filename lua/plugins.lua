d287744e9a2475044a46ee4dafb9692d198f11b8
Francisco Chiotta <franchiotta@gmail.com>
2023-02-26 22:42:06 +0100

    Configure nvim-cmp plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,11 @@
 require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use 'scrooloose/nerdtree'
-  use { 'junegunn/fzf', run = ":call fzf#install()" }
-  use { 'junegunn/fzf.vim' }
+  use {
+      'junegunn/fzf',
+      run = ":call fzf#install()"
+  }
+  use 'junegunn/fzf.vim'
   use {
       'wincent/command-t',
       run = 'cd lua/wincent/commandt/lib && make',
@@ -15,8 +18,23 @@ require('packer').startup(function()
       end,
   }
   use 'ellisonleao/gruvbox.nvim'
+
+  -- lsp
   use 'neovim/nvim-lspconfig'
   use 'williamboman/mason.nvim'
+  use {
+        "hrsh7th/nvim-cmp",
+        requires = {
+            'neovim/nvim-lspconfig',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/cmp-vsnip',
+            'hrsh7th/vim-vsnip'
+        }
+  }
+
   use 'udalov/kotlin-vim'
   use 'Einenlum/yaml-revealer'
   use 'preservim/nerdcommenter'
@@ -29,6 +47,7 @@ require('packer').startup(function()
       setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
       ft = { "markdown" }
   }
+  use 'szw/vim-maximizer'
 end)
 
 -- command-t
@@ -45,6 +64,8 @@ vim.cmd('nnoremap <leader>ntf :NERDTreeFind<CR>')
 vim.cmd('nnoremap <leader>ntfc :NERDTreeFocus<CR>')
 vim.cmd('nnoremap <leader>ntt :NERDTreeToggle<CR>')
 vim.cmd('nnoremap <leader>ntb :NERDTreeFromBookmark ')
+--vim.api.nvim_command('autocmd VimEnter * NERDTree')
+
 vim.g.NERDTreeShowBookmarks = 1
 vim.g.NERDTreeChDirMode = 2
 
@@ -54,6 +75,74 @@ require("gruvbox").setup({
   contrast = "hard"
 })
 vim.cmd("colorscheme gruvbox")
+
+-- vim-airline
+vim.api.nvim_set_var('airline#extensions#tabline#enabled', 1)
+vim.api.nvim_set_var('airline#extensions#branch#enabled', 1)
+vim.g.airline_solarized_bg = 'dark'
+vim.g.airline_powerline_fonts = 1
+
+-- vim-maximizer
+vim.cmd('nnoremap <C-w>m :MaximizerToggle!<CR>')
+
+-- nvim-cmp
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+     vim.fn["vsnip#anonymous"](args.body)
+    end
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- set capabilities in lsp server configs (see below).
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- lsd-config
 local opts = { noremap=true, silent=true }
@@ -85,15 +174,18 @@ local on_attach = function(client, bufnr)
 end
 
 require'lspconfig'.jedi_language_server.setup {
-    on_attach = on_attach
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 require'lspconfig'.jdtls.setup {
-    on_attach = on_attach
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 require'lspconfig'.kotlin_language_server.setup {
-    on_attach = on_attach
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 -- mason
@@ -108,9 +200,5 @@ require("mason").setup({
     }
 })
 
--- vim-airline
-vim.api.nvim_set_var('airline#extensions#tabline#enabled', 1)
-vim.api.nvim_set_var('airline#extensions#branch#enabled', 1)
-vim.g.airline_solarized_bg = 'dark'
-vim.g.airline_powerline_fonts = 1
+-- Don't remove return, otherwise it will not load.
 return
