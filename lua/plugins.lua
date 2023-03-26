549af7ace342b5ec644c57c09bf7c533abfe34f8
Francisco Chiotta <franchiotta@gmail.com>
2023-03-26 12:57:24 +0200

    Configure groovyls, gradle_ls and lua_ls

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -224,17 +224,39 @@ require'lspconfig'.kotlin_language_server.setup {
     capabilities = capabilities
 }
 
--- mason
-require("mason").setup({
-    log_level = vim.log.levels.DEBUG,
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
-})
+require'lspconfig'.groovyls.setup{
+    on_attach = on_attach,
+    capabilities = capabilities
+}
+
+require'lspconfig'.gradle_ls.setup{
+    on_attach = on_attach,
+    capabilities = capabilities
+}
+
+require'lspconfig'.lua_ls.setup {
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 -- Don't remove return, otherwise it will not load.
 return
